871c7142810101d62a469ad9ec181bf02cdac2a8
Baran <baran.solen@gmail.com>
2023-02-28 13:27:32 +0100

    First version

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,44 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/format/DateFormat",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, DateFormat, JSONModel, MessageToast) {
+        "use strict";
+
+        return Controller.extend("project1.controller.MainView", {
+            onInit: function () {
+                var sPath = sap.ui.require.toUrl("project1/localService/mockdata/mock_data.json");
+                var oModel = new JSONModel(sPath);
+                this.getView().setModel(oModel);
+            },
+
+            onFeedPost: function (oEvent) {
+                var oFormat = DateFormat.getDateTimeInstance({ style: "medium" });
+                var oDate = new Date();
+                var sDate = oFormat.format(oDate);
+                var sValue = oEvent.getParameter("value");
+                var oEntry = {
+                    Author: "Author",
+                    Type: "Comment",
+                    Date: "" + sDate,
+                    Text: sValue
+                };
+
+                var oModel = this.getView().getModel();
+                var aEntries = oModel.getData().EntryCollection;
+                aEntries.unshift(oEntry);
+                oModel.setData({
+                    EntryCollection : aEntries
+                });
+            },
+
+            onFooterToolbarPress: function (oEvent) {
+                MessageToast.show(oEvent.getSource().getText());
+            }
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"project1/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
